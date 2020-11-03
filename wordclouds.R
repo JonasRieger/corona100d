073c091508abf5a20374e455b8afa31e3bd71105
JonasRieger <jonas.rieger@tu-dortmund.de>
2020-11-03 14:05:23 +0100

    hashtags gif

--- wordclouds.R
+++ wordclouds.R
@@ -2,6 +2,7 @@ library(data.table)
 library(RColorBrewer)
 #library(wordcloud)
 library(ggwordcloud)
+library(ggplot2)
 library(lubridate)
 
 obj = readRDS(file.path("data", "reducednodups20200706.rds"))
@@ -10,29 +11,29 @@ obj = obj[obj$date > as.Date("2020-03-18") & obj$date < as.Date("2020-03-19")+10
 tmp = table(obj$date)
 
 tab = data.table(date = as.Date(names(tmp)), count = as.numeric(tmp))
-tab = head(tail(tab, -1), -1)
+#tab = head(tail(tab, -1), -1)
 tab[, wday :=  wday(date)] # Sonntag = 1
 
 pdf("counts.pdf", height = 6, width = 10)
 par(mar = c(4.2,4.2,0.5,0.5))
 plot(tab$date, tab$count/1000, type = "h", ylim = c(0, max(tab$count/1000)),
-     xlab = "Date in 2020", ylab = "# Tweets (in thousands)")
+  xlab = "Date in 2020", ylab = "# Tweets (in thousands)")
 col = brewer.pal(8, "Dark2")[2:8]
 for(day in 1:7){
   points(tab[wday == day]$date, tab[wday == day]$count/1000,
-         col = col[day], pch = 20)
+    col = col[day], pch = 20)
 }
 legend("top", horiz = T, c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"),
-       col = col, pch = 20, bty = "n")
+  col = col, pch = 20, bty = "n")
 dev.off()
 
 
 # Hashtag-Wordclouds
 hashtags = lapply(tab$date,
-                  function(x) tolower(unlist(strsplit(obj[date == x]$hashtags, " "))))
+  function(x) tolower(unlist(strsplit(obj[date == x]$hashtags, " "))))
 levels = unique(unlist(lapply(hashtags, unique)))
 hashtagtab = do.call(cbind, lapply(hashtags,
-                                   function(x) table(factor(x, levels = levels))))
+  function(x) table(factor(x, levels = levels))))
 colnames(hashtagtab) = as.character(tab$date)
 rm(obj)
 
@@ -81,7 +82,10 @@ for(i in 1:ncol(hashtagtab)){
   freq = hashtagtab[,i]
   id = freq >= sort(freq, decreasing = TRUE)[25]
   tmp = ggwordcloud(words[id], freq[id]) +
-    ggtitle(paste0(wday(colnames(hashtagtab)[i], label = TRUE), ", ", colnames(hashtagtab)[i]))
+    ggtitle(paste0(colnames(hashtagtab)[i], " (", wday(colnames(hashtagtab)[i], label = TRUE), ")"))
   plot(tmp)
+  #ggsave(plot = tmp, 
+  #       filename = file.path("gif", paste0(colnames(hashtagtab)[i], ".png")), 
+  #       device = "png")
 }
 dev.off()
